e4e2ff8b37464a308f272920c72a63a8496b627c
DJYazan <152520614+DJYazan@users.noreply.github.com>
2024-09-03 11:02:42 +0200

    Update Strings.lua

--- Strings.lua
+++ Strings.lua
@@ -1,5 +1,5 @@
 -- Collection of strings later formatted by the decompiler for output
-local MemeStrings = {"𝓓𝓮𝓬𝓸𝓶𝓹𝓲𝓵𝓮𝓭 𝓫𝔂 𝓯𝓻𝓮𝓪𝓴𝔂 𝓭𝓮𝓬𝓸𝓶𝓹𝓲𝓵𝓮𝓻", "𝔻𝕖𝕔𝕠𝕞𝕡𝕚𝕝𝕖𝕕 𝕨𝕚𝕥𝕙 𝕊𝕜𝕚𝕓𝕚𝕕𝕚 𝔻𝕖𝕔𝕠𝕞𝕡𝕚𝕝𝕖𝕣", "--Decompiled with the Synapse X Luau Decompiler no joke ----"}
+local MemeStrings = {"𝓓𝓮𝓬𝓸𝓶𝓹𝓲𝓵𝓮𝓭 𝓫𝔂 𝓯𝓻𝓮𝓪𝓴𝔂 𝓭𝓮𝓬𝓸𝓶𝓹𝓲𝓵𝓮𝓻", "𝔻𝕖𝕔𝕠𝕞𝕡𝕚𝕝𝕖𝕕 𝕨𝕚𝕥𝕙 𝕊𝕜𝕚𝕓𝕚𝕕𝕚 𝔻𝕖𝕔𝕠𝕞𝕡𝕚𝕝𝕖𝕣", "--Decompiled with the Synapse X Luau Decompiler no joke ----", "decompiled with medal", "decompiled with oracle", "decompiled with top notch decompiler", "You decompiled this @ " .. os.date() .. "took: " .. os.clock()}
 
 local Strings = {
 	SUCCESS = "--" .. MemeStrings[math.random(#MemeStrings)] .. "\n%s",
